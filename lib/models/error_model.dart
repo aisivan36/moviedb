66026799108f9df55bc0003c8f560d5be1f286fa
ivan-boginski <aisivan36@gmail.com>
2022-03-20 19:28:57 +0700

    error model handling

--- lib/models/error_model.dart
+++ lib/models/error_model.dart
@@ -1,5 +1,20 @@
-class FetchDataError {
+import 'package:equatable/equatable.dart';
+
+class FetchDataError extends Equatable {
   final String message;
 
-  const FetchDataError(this.message);
+  const FetchDataError({required this.message});
+
+  factory FetchDataError.set(String err) {
+    return FetchDataError(message: err);
+  }
+
+  String get getError => message;
+
+  // void setErr(String err) {
+  //   message = err;
+  // }
+
+  @override
+  List<Object> get props => [message];
 }
