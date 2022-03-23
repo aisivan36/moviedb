3be32470af137a92581ee950693929a789ba6823
ivan-boginski <aisivan36@gmail.com>
2022-03-23 11:06:31 +0700

    fixed

--- lib/widgets/horizontal_list_cards.dart
+++ lib/widgets/horizontal_list_cards.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:movie_db/animation.dart';
 
 import 'package:movie_db/models/movies/movie_model.dart';
 import 'package:movie_db/models/tv_models/tv_model.dart';
+import 'package:movie_db/screens/movie_detail_screen/movie_detail_screen.dart';
 import 'package:movie_db/widgets/movie_cards.dart';
 
 /// Horizontal List View Movies
@@ -12,24 +14,6 @@ class HorizontalListViewMovies extends StatelessWidget {
   final List<MovieModel> list;
   final Color? color;
 
-  /// Movie Card
-  List<Widget> movie() {
-    return list.map((dataMovie) {
-      return MovieCard(
-        poster: dataMovie.poster,
-        name: dataMovie.title,
-        backdrop: dataMovie.backdrop,
-        date: dataMovie.releaseDate,
-        id: dataMovie.id,
-        color: color == null ? Colors.white : color!,
-        isMovie: true,
-        onTap: () {
-          // TODO PushNewScreen to MovieDetailsScreen
-        },
-      );
-    }).toList();
-  }
-
   @override
   Widget build(BuildContext context) {
     return SizedBox(
@@ -39,20 +23,35 @@ class HorizontalListViewMovies extends StatelessWidget {
         physics: const BouncingScrollPhysics(),
         scrollDirection: Axis.horizontal,
         children: [
-          SizedBox(width: 7),
-          for (int i = 0; i < list.length; i++)
-            MovieCard(
-              poster: list[i].poster,
-              name: list[i].title,
-              backdrop: list[i].backdrop,
-              date: list[i].releaseDate,
-              id: list[i].id,
+          const SizedBox(width: 7),
+          // for (int i = 0; i < list.length; i++)
+          //   MovieCard(
+          //     poster: list[i].poster,
+          //     name: list[i].title,
+          //     backdrop: list[i].backdrop,
+          //     date: list[i].releaseDate,
+          //     id: list[i].id,
+          //     color: color == null ? Colors.white : color!,
+          //     isMovie: true,
+          //     onTap: () {
+          //
+          //     },
+          //   ),
+          ...list.map(
+            (data) => MovieCard(
+              poster: data.poster,
+              name: data.title,
+              backdrop: data.backdrop,
+              date: data.releaseDate,
+              id: data.id,
               color: color == null ? Colors.white : color!,
               isMovie: true,
               onTap: () {
-                // TODO PushNewScreen to MovieDetailsScreen
+                pushNewScreen(context,
+                    MovieDetailScreen(backdrop: data.backdrop, id: data.id));
               },
             ),
+          ),
         ],
       ),
     );
