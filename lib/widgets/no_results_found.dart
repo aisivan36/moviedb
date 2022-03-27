a413c2b5530c9fc4873fcd9d2aad3b9357fb6d38
ivan-boginski <aisivan36@gmail.com>
2022-03-27 08:39:21 +0700

    no result

--- lib/widgets/no_results_found.dart
+++ lib/widgets/no_results_found.dart
@@ -41,3 +41,42 @@ class NoResultsFound extends StatelessWidget {
     );
   }
 }
+
+class EmptyFavorites extends StatelessWidget {
+  const EmptyFavorites({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.8,
+      padding: const EdgeInsets.all(20.0),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Icon(
+              Icons.favorite,
+              size: 45,
+              color: Colors.white,
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'Your Favorites is Empty',
+              style: heading.copyWith(
+                color: Colors.white.withOpacity(0.9),
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'Add Your Favorites movies and tv shows to track your favorites list.',
+              textAlign: TextAlign.center,
+              style: normalText.copyWith(
+                color: Colors.white.withOpacity(0.9),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
