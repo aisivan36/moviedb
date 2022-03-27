0895c1885cb05a533f978607bf58e79ad28bd8e0
ivan-boginski <aisivan36@gmail.com>
2022-03-27 17:51:06 +0700

    fixed

--- lib/screens/tvshow_detail_screen/bloc/tv_show_detail_bloc.dart
+++ lib/screens/tvshow_detail_screen/bloc/tv_show_detail_bloc.dart
@@ -1,6 +1,5 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:flutter/gestures.dart';
 import 'package:movie_db/models/error_model.dart';
 import 'package:movie_db/models/movies/cast_info_model.dart';
 import 'package:movie_db/models/movies/image_backdrop_model.dart';
